b437bbf98e6582652f9a076c28093432d8262da4
kiran04-code <kr551344@gmail.com>
2026-04-12 04:41:59 +0530

    fix(secrets): move hardcoded secrets to environment variables

--- server/src/client/redis/index.ts
+++ server/src/client/redis/index.ts
@@ -1,9 +1,10 @@
 import { Redis } from "ioredis";
 
 export const redisclient = new Redis(
-  "rediss://default:AXRNAAIncDI2NmRiOWIwNDdjYjg0ZjNjYTQwMDk2ZTVjNDEzODRmOHAyMjk3NzM@pure-gecko-29773.upstash.io:6379",
+  "rediss://default:import.meta.env.VITE_INDEX_HIGH_ENTROPY_VALUE@pure-gecko-29773.upstash.io:6379",
   {
     enableReadyCheck: false,
     maxRetriesPerRequest: null,
   }
 );
+
