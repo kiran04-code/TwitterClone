810daaf08bf1db850d5146dfed87fa59df6d250e
kiran04-code <kr551344@gmail.com>
2026-04-12 05:14:59 +0530

    fix(secrets): move hardcoded secrets to environment variables

--- twitter-clinet/components/RightSide/RightsideFeed.tsx
+++ twitter-clinet/components/RightSide/RightsideFeed.tsx
@@ -9,7 +9,7 @@ import { useCurrentUsert } from '@/hooks/user'
 
 const RightsideFeed = () => {
    const user = useCurrentUsert()
-   const Api_key = "uMhmjknx9eGIgpyQdLa3VekNmPc"
+   const Api_key = process.env.RIGHTSIDEFEED_HIGH_ENTROPY_VALUE
    return (
       <div className='col-span-3'>
          <SearchBar />
@@ -27,4 +27,5 @@ const RightsideFeed = () => {
 }
 
 export default RightsideFeed
+
 
