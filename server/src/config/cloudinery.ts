0848ce993f508547ce6a31e38813d69abd607695
kiran04-code <kr551344@gmail.com>
2026-04-12 03:18:22 +0530

    fix(secrets): move hardcoded secrets to environment variables

--- server/src/config/cloudinery.ts
+++ server/src/config/cloudinery.ts
@@ -1,10 +1,12 @@
+import "dotenv/config";
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
   cloud_name: "dltg47wml",
   api_key: "872274738955133",
-  api_secret: "uMhmjknx9eGIgpyQdLa3VekNmPc",
+  api_secret: process.env.CLOUDINERY_HIGH_ENTROPY_VALUE,
   secure: true,
 });
 
 export default cloudinary;
+
